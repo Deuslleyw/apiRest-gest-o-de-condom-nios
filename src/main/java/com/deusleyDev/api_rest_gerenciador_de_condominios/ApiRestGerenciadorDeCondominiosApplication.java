9d0d84397904778f31ab6c94bed37cd615fb7396
Deuslleyw <Deuslleyw@hotmail.com>
2024-10-11 14:50:20 -0300

    firstCommit

---
+++ src/main/java/com/deusleyDev/api_rest_gerenciador_de_condominios/ApiRestGerenciadorDeCondominiosApplication.java
@@ -0,0 +1,13 @@
+package com.deusleyDev.api_rest_gerenciador_de_condominios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestGerenciadorDeCondominiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestGerenciadorDeCondominiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deusleyDev/api_rest_gerenciador_de_condominios/ApiRestGerenciadorDeCondominiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deusleyDev.api_rest_gerenciador_de_condominios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestGerenciadorDeCondominiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
